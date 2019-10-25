ed31d7a6b743e72d13aaad61860b733ae6684eb7
Brosir-Maraka <brosirm@gmail.com>
2019-10-25 15:19:02 +0300

    add calculator

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+var add= function(number, number2) {return number1+ number2};
+var number1 = parseInt (prompt ("Enter a number:"));
+var number2 =parseInt (prompt ("enter another number;"));
+var result = add(number,number2);
+alert(result)
