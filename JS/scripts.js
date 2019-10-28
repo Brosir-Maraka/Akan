627ff2f194bf43e25e8dc851151d21d32cb67afe
Brosir-Maraka <brosirm@gmail.com>
2019-10-28 23:59:03 +0300

    add akan names

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,41 @@
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Ama"];
+var maleNames = [" Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var days = [
+  "sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesady",
+  "Thursay",
+  "FRiday",
+  "Satruday"
+];
 
-var add= function(number, number2) {return number1+ number2};
-var number1 = parseInt (prompt ("Enter a number:"));
-var number2 =parseInt (prompt ("enter another number;"));
-var result = add(number,number2);
-alert(result)
+function akanNames() {
+  console.log("clicked");
+  var year = parseInt(document.getElementById("yy").value);
+  var month = parseInt(document.getElementById("mm").value);
+  var day = parseInt(document.getElementById("dd").value);
+  var male = document.getElementById("M");
+  var female = document.getElementById("F");
+
+  if (day < 1 || day > 32) {
+    alert("error");
+  }
+  var day = new Date(year + "/" + month + "/" + day);
+  var myDay = day.getDay();
+  if (female.checked == true) {
+    alert(
+      "Great, you were born on " +
+        days[myDay] +
+        " your akan name is " +
+        femaleNames[myDay]
+    );
+  } else if (male.checked == true) {
+    alert(
+      "Great, you were born on " +
+        days[myDay] +
+        " your akan name is " +
+        maleNames[myDay]
+    );
+  }
+}
